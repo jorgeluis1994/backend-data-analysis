a343d8e8cebc54028254e42f6f082ede105ba3ee
Jorge Luis <jorgeluis199454@gmail.com>
2025-03-18 17:07:11 -0500

    view

---
+++ script/titularidad.sql
@@ -0,0 +1,166 @@
+SELECT 
+    dc.center_identifier,
+    UPPER(dc.center_name) as center_name,
+    UPPER(dc.manager_legal_personality) as manager_legal_personality,
+    UPPER(p.occupied_place_type ) as occupied_place_type,
+    --Numero de plazas concertadas
+    dc.num_contracted_places,
+    -- Total de usuarios
+    COUNT(*) AS total_users,
+    -- Promedio de edad
+    ROUND(AVG(EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.date_of_birth)))::numeric, 1) AS average_age,
+    -- Promedio de altura
+    ROUND(AVG(p.height_cm::numeric), 1) AS average_height,
+    -- Promedio de peso
+    ROUND(AVG(p.last_weight::numeric), 1) AS average_weight,
+    -- Cálculo del IMC (evitar división por cero)
+    ROUND(
+        AVG(CASE WHEN p.height_cm > 0 THEN p.last_weight::numeric / (POW(p.height_cm / 100, 2)) ELSE NULL END)::numeric, 
+        1
+    ) AS average_imc,  
+    -- Desviación estándar para la edad
+    ROUND(STDDEV(EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.date_of_birth)))::numeric, 1) AS stddev_age,
+    -- Desviación estándar para la altura
+    ROUND(STDDEV(p.height_cm::numeric), 1) AS stddev_height,
+    -- Desviación estándar para el IMC (agregado correctamente)
+    ROUND(STDDEV(
+        CASE WHEN p.height_cm > 0 THEN p.last_weight::numeric / (POW(p.height_cm / 100, 2)) ELSE NULL END
+    )::numeric, 1) AS stddev_imc,
+    -- Porcentaje de datos válidos en toda la data (considerando todas las columnas relevantes)
+    ROUND((
+    (COUNT(*) 
+    - COUNT(CASE WHEN p.date_of_birth IS NULL THEN 1 END)
+    - COUNT(CASE WHEN p.height_cm IS NULL THEN 1 END)
+    - COUNT(CASE WHEN p.last_weight IS NULL THEN 1 END)
+    - COUNT(CASE WHEN p.gender IS NULL THEN 1 END)
+    - COUNT(CASE WHEN p.center_identifier IS NULL THEN 1 END)
+    -- Agrega más columnas que quieras verificar como nulas
+    ) * 100.0) / NULLIF(COUNT(*), 0), 1) AS valid_data_percentage
+FROM 
+    public.dim_people p
+JOIN 
+    public.dim_centers dc ON p.center_identifier = dc.center_identifier
+GROUP BY 
+    dc.center_identifier, 
+    dc.center_name,
+    dc.manager_legal_personality,
+    p.occupied_place_type,
+    dc.num_contracted_places
+ORDER BY 
+    average_age DESC NULLS LAST;	
+
+
+
+SELECT
+    p.center_identifier,
+    p.manager_legal_personality,
+    p.occupied_place_type,
+    UPPER(p.center_name) as center_name,
+    SUM(p.total_users) AS total_users,
+    ROUND(AVG(p.average_age)::numeric, 2) AS avg_age,
+    ROUND(STDDEV(p.average_age)::numeric, 2) AS stddev_age,
+    ROUND(AVG(p.average_height)::numeric, 2) AS avg_height,
+    ROUND(STDDEV(p.average_height)::numeric, 2) AS stddev_height,
+    ROUND(AVG(p.average_weight)::numeric, 2) AS avg_weight,
+    ROUND(STDDEV(p.average_weight)::numeric, 2) AS stddev_weight
+FROM
+    catalog_statistics_centers_people p
+GROUP BY
+    p.center_identifier,
+    p.center_name,
+    p.occupied_place_type,
+    p.manager_legal_personality
+ORDER BY
+    p.center_identifier;
+
+
+
+SELECT 
+    dc.center_identifier,
+    UPPER(dc.center_name) as center_name,
+    UPPER(dc.manager_legal_personality) as manager_legal_personality,
+    UPPER(p.occupied_place_type ) as occupied_place_type,
+    -- Número de plazas concertadas
+    dc.num_contracted_places,
+    -- Total de usuarios
+    COUNT(*) AS total_users,
+    -- Promedio de edad
+    ROUND(AVG(EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.date_of_birth)))::numeric, 1) AS average_age,
+    -- Promedio de altura
+    ROUND(AVG(p.height_cm::numeric), 1) AS average_height,
+    -- Promedio de peso
+    ROUND(AVG(p.last_weight::numeric), 1) AS average_weight,
+    -- Cálculo del IMC (evitar división por cero)
+    ROUND(
+        AVG(CASE WHEN p.height_cm > 0 THEN p.last_weight::numeric / (POW(p.height_cm / 100, 2)) ELSE NULL END)::numeric, 
+        1
+    ) AS average_imc,  
+    -- Desviación estándar para la edad
+    ROUND(STDDEV(EXTRACT(YEAR FROM AGE(CURRENT_DATE, p.date_of_birth)))::numeric, 1) AS stddev_age,
+    -- Desviación estándar para la altura
+    ROUND(STDDEV(p.height_cm::numeric), 1) AS stddev_height,
+    -- Desviación estándar para el IMC (agregado correctamente)
+    ROUND(STDDEV(
+        CASE WHEN p.height_cm > 0 THEN p.last_weight::numeric / (POW(p.height_cm / 100, 2)) ELSE NULL END
+    )::numeric, 1) AS stddev_imc,
+    -- Porcentaje de datos válidos en toda la data (considerando todas las columnas relevantes)
+    ROUND(((
+        COUNT(*) 
+        - COUNT(CASE WHEN p.date_of_birth IS NULL THEN 1 END)
+        - COUNT(CASE WHEN p.height_cm IS NULL THEN 1 END)
+        - COUNT(CASE WHEN p.last_weight IS NULL THEN 1 END)
+        - COUNT(CASE WHEN p.gender IS NULL THEN 1 END)
+        - COUNT(CASE WHEN p.center_identifier IS NULL THEN 1 END)
+        -- Agrega más columnas que quieras verificar como nulas
+    ) * 100.0) / NULLIF(COUNT(*), 0), 1) AS valid_data_percentage
+FROM 
+    public.dim_people p
+JOIN 
+    public.dim_centers dc ON p.center_identifier = dc.center_identifier
+WHERE
+    UPPER(dc.center_name) = 'ALDAY'  -- Filtra por el nombre de centro "ALDAY"
+GROUP BY 
+    dc.center_identifier, 
+    dc.center_name,
+    dc.manager_legal_personality,
+    p.occupied_place_type,
+    dc.num_contracted_places
+ORDER BY 
+    average_age DESC NULLS LAST;
+
+select * from catalog_statistics_centers_people
+
+--Consulta por TIPO
+SELECT
+    p.occupied_place_type,
+    SUM(p.total_users) AS total_users,
+    ROUND(AVG(p.average_age)::numeric, 2) AS avg_age,
+    ROUND(STDDEV(p.average_age)::numeric, 2) AS stddev_age,
+    ROUND(AVG(p.average_height)::numeric, 2) AS avg_height,
+    ROUND(STDDEV(p.average_height)::numeric, 2) AS stddev_height,
+    ROUND(AVG(p.average_weight)::numeric, 2) AS avg_weight,
+    ROUND(STDDEV(p.average_weight)::numeric, 2) AS stddev_weight
+FROM
+    catalog_statistics_centers_people p
+WHERE
+    p.manager_legal_personality LIKE '%PUBLICA%'  -- Filtra por la palabra "publica"
+GROUP BY
+    p.occupied_place_type;
+
+CREATE VIEW view_titularidad AS
+SELECT
+    p.manager_legal_personality,
+    SUM(p.total_users) AS total_users,
+    ROUND(AVG(p.average_age)::numeric, 2) AS avg_age,
+    ROUND(STDDEV(p.average_age)::numeric, 2) AS stddev_age,
+    ROUND(AVG(p.average_height)::numeric, 2) AS avg_height,
+    ROUND(STDDEV(p.average_height)::numeric, 2) AS stddev_height,
+    ROUND(AVG(p.average_weight)::numeric, 2) AS avg_weight,
+    ROUND(STDDEV(p.average_weight)::numeric, 2) AS stddev_weight
+FROM
+    catalog_statistics_centers_people p
+GROUP BY
+    p.manager_legal_personality;
+
+SELECT * 
+FROM view_titularidad
